8c5993b9de8e9a6312990f8b9f9fb814767e1e9c
zombie <re2=cool!>
2014-04-14 12:23:45 +0200

    Refactored code to allow usage in website. Added little website demo.

--- src/ObservablePO.js
+++ src/ObservablePO.js
@@ -1,4 +1,21 @@
-(function(target) {
+//Copyright 2014 Martin Keiblinger
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//limitations under the License.
+"use strict";
+
+var module = module || { exports : {} };
+
+var ObservablePO = (function(target) {
     function ObservablePO(data, subscribers) {
         if(isNullOrPrimitive(data)) {
             throw {
@@ -60,7 +77,7 @@
     }
 
     function setupGetterSetter(instanceObj, data) {
-        for(key in data) {
+        for(var key in data) {
             if(data.hasOwnProperty(key)) {
                 var firstChar = key.charAt(0).toUpperCase(),
                     name = firstChar + key.substr(1);
@@ -86,4 +103,6 @@
     module.exports.ObservablePO = ObservablePO;
     target = target || {};
     target = target.ObservablePO = ObservablePO;
+
+    return ObservablePO;
 })();
